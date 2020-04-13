65cf4a902d2fd51860b548d1c5a0cb61c51a10e3
Phyllipe <61892319+Phyllipe@users.noreply.github.com>
2020-04-12 22:07:09 -0300

    Add files via upload

---
+++ at marlao 1/at marlao 1/CALCULADORA.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace at_marlao_1
+{
+    class CALCULADORA
+    { private new List<double> listNumero = new List<double>();
+
+            public void AddNumero(double num)
+            {
+                this.listNumero.Add(num);
+
+            }
+            public double Somar()
+            {
+                double resultado = 0;
+                foreach (double numero in this.listNumero)
+                {
+                    resultado += numero;
+                }
+                this.listNumero.Clear();
+                return resultado;
+            }
+            public double Subtrair()
+            {
+                double resultado = this.listNumero[0];
+                this.listNumero.RemoveAt(0);
+                foreach (double numero in this.listNumero)
+                {
+                    resultado -= numero;
+                }
+                this.listNumero.Clear();
+                return resultado;
+            }
+            public double Multi()
+            {
+                double resultado = this.listNumero[0];
+                this.listNumero.RemoveAt(0);
+                foreach (double numero in this.listNumero)
+                {
+
+                    resultado *= numero;
+                }
+                this.listNumero.Clear();
+                return resultado;
+            }
+            public double Divisao()
+            {
+                double resultado = this.listNumero[0];
+                this.listNumero.RemoveAt(0);
+                foreach (double numero in this.listNumero)
+                {
+
+                    resultado /= numero;
+                }
+                this.listNumero.Clear();
+                return resultado;
+             }
+        public double quadrado()
+        {
+            double resultado = this.listNumero[0];
+            
+            foreach (double numero in this.listNumero)
+            {
+
+                resultado =Math.Pow( numero, 2);
+            }
+            this.listNumero.Clear();
+        
+            return resultado;
+        }
+
+
+
+
+    }
+        }
+
+    
+
+
+
---
+++ at marlao 1/at marlao 1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace at_marlao_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CALCULADORA x = new CALCULADORA();
+            Console.WriteLine("soma");
+            x.AddNumero(Convert.ToDouble(Console.ReadLine()));
+            x.AddNumero(Convert.ToDouble(Console.ReadLine()));
+
+            System.Console.WriteLine(x.Somar());
+
+
+            Console.WriteLine("multiplicação");
+            x.AddNumero(Convert.ToDouble(Console.ReadLine()));
+            x.AddNumero(Convert.ToDouble(Console.ReadLine()));
+
+            System.Console.WriteLine(x.Multi());
+
+
+            Console.WriteLine("subtração");
+            x.AddNumero(Convert.ToDouble(Console.ReadLine()));
+            x.AddNumero(Convert.ToDouble(Console.ReadLine()));
+
+            System.Console.WriteLine(x.Subtrair());
+
+
+            Console.WriteLine("divisão");
+            x.AddNumero(Convert.ToDouble(Console.ReadLine()));
+            x.AddNumero(Convert.ToDouble(Console.ReadLine()));
+
+            System.Console.WriteLine(x.Divisao());
+
+
+            Console.WriteLine("Quadrado");
+            x.AddNumero(Convert.ToDouble(Console.ReadLine()));
+            
+
+
+            System.Console.WriteLine(x.quadrado());
+            
+
+
+
+
+
+
+
+        }
+    }
+}
